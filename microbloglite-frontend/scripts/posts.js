756c7f8848405f1e128801263b93f9ee6fb65e62
Enki Hazzard <ehazzard@appdev.yearup.org>
2024-12-18 14:41:19 -0500

    posting_progress

--- microbloglite-frontend/scripts/posts.js
+++ microbloglite-frontend/scripts/posts.js
@@ -1,6 +1,7 @@
 /* Posts Page JavaScript */
 
 "use strict";
+//  Load multiple posts from the api
 
 async function getPosts() {
   const loginData = getLoginData();
@@ -19,7 +20,19 @@ async function getPosts() {
     console.log(data);
   } catch (error) {
     console.error(error.message);
-    
   }
 }
+function displayPosts(posts) {
+  const container = document.getElementById("posts-container");
+  container.innerHTML = ""; // this will clear any exisiting content
+
+  posts.forEach ((post) => {
+    const postQuack = document.createElement("div");
+    postQuack.classList.add(col-md-4);
+
+    container.appendChild(postQuack)
+  }
+  )
+}
+
 getPosts();
